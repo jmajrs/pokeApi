c0cd9ecd92089e8ac47c18cf1a34873301ae5610
jmajrs <jmajrs@gmail.com>
2022-11-07 16:16:28 -0600

    Added function get pokemon

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const cardContainer = document.querySelector('.section')
+
+const getPokemon = function (data, className = '') {
+    const html = `<div class="card-container">
+    <div class="description">
+        <img>
+        <p class="name"><strong>Name</strong> ##hp</p>
+        <p class="xp">##xp</p>
+    </div>
+    <div class="stats">
+        <p class="attack">
+            <strong>##</strong>
+            Attack
+        </p>
+        <p class="defense">
+            <strong>##</strong>
+            Defense
+    </div>
+    </p>
+</div>`
+
+    cardContainer.insertAdjacentHTML('beforeend', html);
+    cardContainer.style.opacity = 1;
+};
+
+getPokemon();
