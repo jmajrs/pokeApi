2115b8d5a73ce175fa2dfb678d44ca7a17f5ff76
jmajrs <jmajrs@gmail.com>
2022-11-07 17:22:40 -0600

    Updated getPokemon function

--- script.js
+++ script.js
@@ -2,27 +2,37 @@
 
 const cardContainer = document.querySelector('.section')
 
-const getPokemon = function (data, className = '') {
-    const html = `<div class="card-container">
+
+const getPokemon = function (pokemon) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    request.send();
+
+    request.addEventListener('load', function () {
+        const data = JSON.parse(this.responseText)
+        console.log(data);
+
+        const html = `
+    <div class="card-container">
     <div class="description">
-        <img>
-        <p class="name"><strong>Name</strong> ##hp</p>
-        <p class="xp">##xp</p>
+        <img src="${data.sprites.other.dream_world.front_default}">
+        <p class="name"><strong>${data.name}</strong> ${data.stats[0].base_stat}hp</p>
+        <p class="xp">${data.base_experience}xp</p>
     </div>
     <div class="stats">
         <p class="attack">
-            <strong>##</strong>
+            <strong>${data.stats[1].base_stat}</strong>
             Attack
         </p>
         <p class="defense">
-            <strong>##</strong>
+            <strong>${data.stats[2].base_stat}</strong>
             Defense
     </div>
     </p>
-</div>`
+</div>`;
+        cardContainer.insertAdjacentHTML('beforeend', html);
+    });
 
-    cardContainer.insertAdjacentHTML('beforeend', html);
-    cardContainer.style.opacity = 1;
 };
 
-getPokemon();
+getPokemon('charizard');
