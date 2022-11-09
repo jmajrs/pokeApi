b588716f9ac352f0cd2bb4343a9900f27334e406
jmajrs <jmajrs@gmail.com>
2022-11-09 17:41:21 -0600

    Added fusioned stats

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const createPokemon = function (data) {
     const name = data.name[0].toUpperCase();
     data.name = name + data.name.slice(1);
 
+    const typeName1 = data.types[0].type.name[0].toUpperCase();
+    data.types[0].type.name = typeName1 + data.types[0].type.name.slice(1);
+
     const html = `
     <div class="card-container">
     <div class="description">
@@ -30,8 +33,13 @@ const createPokemon = function (data) {
         <p class="defense">
             <strong>${data.stats[2].base_stat}</strong>
             Defense
+        </p>
     </div>
-    </p>
+    <div class="types">
+        <p class="attack">
+            ${data.types[0].type.name}
+        </p>
+    </div>
 </div>`;
     cardContainer.insertAdjacentHTML('beforeend', html);
 }
@@ -48,11 +56,23 @@ button.addEventListener('click', async function () {
     createPokemon(pokemonData2);
     // createPokemon(pokemonData3); 
 
+    // Fusioned pokemon
     const fusionName = pokemonData1.name.slice(0, 4) + pokemonData2.name.slice(-4);
+
+    const hpFusion = pokemonData1.stats[0].base_stat + pokemonData2.stats[0].base_stat;
+    pokemonData2.stats[0].base_stat = hpFusion;
+
+    const expFusion = pokemonData1.base_experience + pokemonData2.base_experience
+
     const attackFusion = pokemonData1.stats[1].base_stat + pokemonData2.stats[1].base_stat;
     pokemonData2.stats[1].base_stat = attackFusion;
+
     const deffenseFusion = pokemonData1.stats[2].base_stat + pokemonData2.stats[2].base_stat;
     pokemonData2.stats[2].base_stat = deffenseFusion;
-    createPokemon({ ...pokemonData2, name: fusionName, });
+
+    const typesFusion = pokemonData1.types[0].type.name + '/' + pokemonData2.types[0].type.name;
+    pokemonData2.types[0].type.name = typesFusion;
+
+    createPokemon({ ...pokemonData2, name: fusionName, base_experience: expFusion });
 });
 
